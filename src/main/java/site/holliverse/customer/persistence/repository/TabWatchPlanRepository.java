13d4b2125555fa8c111da76eaa518c5e96673de3
rettooo <gkdud9236@naver.com>
2026-02-14 17:51:36 +0900

    [HSC-61] feat: 상품/요금제 repo 추가

---
+++ src/main/java/site/holliverse/customer/persistence/repository/TabWatchPlanRepository.java
@@ -0,0 +1,7 @@
+package site.holliverse.customer.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.holliverse.customer.persistence.entity.TabWatchPlan;
+
+public interface TabWatchPlanRepository extends JpaRepository<TabWatchPlan, Long> {
+}

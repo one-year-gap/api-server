0496209d15358189e999b5a55f6c02baf538a254
rettooo <gkdud9236@naver.com>
2026-02-14 14:08:29 +0900

    [HSC-60] feat: 부가 서비스 엔티티

---
+++ src/main/java/site/holliverse/customer/persistence/entity/AddonType.java
@@ -0,0 +1,10 @@
+package site.holliverse.customer.persistence.entity;
+
+/**
+ * 부가서비스 타입 (addon.addon_type).
+ */
+public enum AddonType {
+    FAMILY_CARE,   // 가족케어
+    PHONE_CARE,    // 휴대폰케어
+    DIGITAL       // 디지털 콘텐츠
+}

473ac54a2d6b00c5709b287e169804d0f3363435
rettooo <gkdud9236@naver.com>
2026-02-14 17:56:26 +0900

    [HSC-61] feat: Repo에 목록 조회 시 해당 타입 상세 일괄 조회 (N+1방지)

--- src/main/java/site/holliverse/customer/persistence/repository/AddonRepository.java
+++ src/main/java/site/holliverse/customer/persistence/repository/AddonRepository.java
@@ -3,5 +3,9 @@ package site.holliverse.customer.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.holliverse.customer.persistence.entity.Addon;
 
+import java.util.List;
+
 public interface AddonRepository extends JpaRepository<Addon, Long> {
+
+    List<Addon> findByProductIdIn(List<Long> productIds);
 }
--- src/main/java/site/holliverse/customer/persistence/repository/TabWatchPlanRepository.java
+++ src/main/java/site/holliverse/customer/persistence/repository/TabWatchPlanRepository.java
@@ -3,5 +3,9 @@ package site.holliverse.customer.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.holliverse.customer.persistence.entity.TabWatchPlan;
 
+import java.util.List;
+
 public interface TabWatchPlanRepository extends JpaRepository<TabWatchPlan, Long> {
+
+    List<TabWatchPlan> findByProductIdIn(List<Long> productIds);
 }

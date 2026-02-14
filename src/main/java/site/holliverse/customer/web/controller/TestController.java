fe1ae077cdfa555d33496d82243a68fcd2b91764
tkv00 <tkv0098@gmail.com>
2026-02-14 23:13:42 +0900

    [HSC-62] feat: API url 아키텍처 규칙 36에 맞게 수정

--- src/main/java/site/holliverse/customer/web/controller/TestController.java
+++ src/main/java/site/holliverse/customer/web/controller/TestController.java
@@ -2,12 +2,14 @@ package site.holliverse.customer.web.controller;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Profile("customer")
+@RequestMapping("/api/customer")
 public class TestController {
-    @GetMapping("/api/test")
+    @GetMapping("test")
     public String testController() {
         return "customer server";
     }

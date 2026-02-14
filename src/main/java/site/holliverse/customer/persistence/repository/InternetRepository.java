a170ffdcc7594ebc5d2aed6480a034c468b44ddf
rettooo <gkdud9236@naver.com>
2026-02-14 17:56:26 +0900

    feat/HSC-61: Repo에 목록 조회 시 해당 타입 상세 일괄 조회 (N+1방지)

--- src/main/java/site/holliverse/customer/persistence/repository/InternetRepository.java
+++ src/main/java/site/holliverse/customer/persistence/repository/InternetRepository.java
@@ -3,5 +3,9 @@ package site.holliverse.customer.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.holliverse.customer.persistence.entity.Internet;
 
+import java.util.List;
+
 public interface InternetRepository extends JpaRepository<Internet, Long> {
+
+    List<Internet> findByProductIdIn(List<Long> productIds);
 }
--- src/main/java/site/holliverse/customer/persistence/repository/IptvRepository.java
+++ src/main/java/site/holliverse/customer/persistence/repository/IptvRepository.java
@@ -3,5 +3,9 @@ package site.holliverse.customer.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.holliverse.customer.persistence.entity.Iptv;
 
+import java.util.List;
+
 public interface IptvRepository extends JpaRepository<Iptv, Long> {
+
+    List<Iptv> findByProductIdIn(List<Long> productIds);
 }

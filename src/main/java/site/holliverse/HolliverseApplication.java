50866629fbc58a4c4db33b3c0aa203a8230cad35
rettooo <gkdud9236@naver.com>
2026-02-14 14:27:23 +0900

    [HSC-60] feat: fix from review

--- src/main/java/site/holliverse/HolliverseApplication.java
+++ src/main/java/site/holliverse/HolliverseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-        scanBasePackages = "com.holliverse"
+        scanBasePackages = "site.holliverse"
 )
 public class HolliverseApplication {
 

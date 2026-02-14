d23da338aa27223701e2cf88f5ba373be13ef41a
rettooo <gkdud9236@naver.com>
2026-02-15 00:27:21 +0900

    [HSC-65] feat: Content 5개 카테고리 - 각 record 정의

---
+++ src/main/java/site/holliverse/customer/web/dto/MobileContent.java
@@ -0,0 +1,13 @@
+package site.holliverse.customer.web.dto;
+
+public record MobileContent(
+        String data_amount,
+        Integer tethering_sharing_data,
+        String benefit_brands,
+        String benefit_voice_call,
+        String benefit_sms,
+        String benefit_media,
+        String benefit_premium,
+        String benefit_signature_family_discount
+) implements ProductContent {
+}

802abef3c84e8532464175c3d8b8aa8daf59b70d
rettooo <gkdud9236@naver.com>
2026-02-15 20:19:01 +0900

    feat/HSC-65: 카멜식 변경

--- src/main/java/site/holliverse/customer/web/dto/product/MobileContent.java
+++ src/main/java/site/holliverse/customer/web/dto/product/MobileContent.java
@@ -1,13 +1,13 @@
 package site.holliverse.customer.web.dto.product;
 
 public record MobileContent(
-        String data_amount,
-        Integer tethering_sharing_data,
-        String benefit_brands,
-        String benefit_voice_call,
-        String benefit_sms,
-        String benefit_media,
-        String benefit_premium,
-        String benefit_signature_family_discount
+        String dataAmount,
+        Integer tetheringSharingData,
+        String benefitBrands,
+        String benefitVoiceCall,
+        String benefitSms,
+        String benefitMedia,
+        String benefitPremium,
+        String benefitSignatureFamilyDiscount
 ) implements ProductContent {
 }

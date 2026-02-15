f4d3f659ec7f166219b1ed743755482675cdedca
rettooo <gkdud9236@naver.com>
2026-02-15 20:12:07 +0900

    [HSC-65] feat: Assembler 분리

--- src/main/java/site/holliverse/customer/web/mapper/ProductResponseMapper.java
+++ src/main/java/site/holliverse/customer/web/mapper/ProductResponseMapper.java
@@ -2,62 +2,24 @@ package site.holliverse.customer.web.mapper;
 
 import org.springframework.stereotype.Component;
 import site.holliverse.customer.application.usecase.ProductDetailResult;
-import site.holliverse.customer.application.usecase.ProductListResult;
 import site.holliverse.customer.application.usecase.dto.AddonDetailDto;
 import site.holliverse.customer.application.usecase.dto.InternetDetailDto;
 import site.holliverse.customer.application.usecase.dto.IptvDetailDto;
 import site.holliverse.customer.application.usecase.dto.MobilePlanDetailDto;
 import site.holliverse.customer.application.usecase.dto.ProductSummaryDto;
 import site.holliverse.customer.application.usecase.dto.TabWatchPlanDetailDto;
-import site.holliverse.customer.web.dto.PageMeta;
 import site.holliverse.customer.web.dto.product.AddonContent;
 import site.holliverse.customer.web.dto.product.InternetContent;
 import site.holliverse.customer.web.dto.product.IptvContent;
 import site.holliverse.customer.web.dto.product.MobileContent;
 import site.holliverse.customer.web.dto.product.ProductContent;
 import site.holliverse.customer.web.dto.product.ProductDetailResponse;
-import site.holliverse.customer.web.dto.product.ProductListResponse;
 import site.holliverse.customer.web.dto.product.TabWatchContent;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class ProductResponseMapper {
 
-    public ProductListResponse toListResponse(ProductListResult result) {
-        Map<Long, ProductContent> contentByProductId = buildContentMap(result);
-        List<ProductDetailResponse> content = result.products().getContent().stream()
-                .map(p -> toDetailResponse(p, contentByProductId.get(p.productId())))
-                .toList();
-        PageMeta page = new PageMeta(
-                result.products().getTotalElements(),
-                result.products().getTotalPages(),
-                result.products().getNumber(),
-                result.products().getSize()
-        );
-        return new ProductListResponse(page, content);
-    }
-
-    private Map<Long, ProductContent> buildContentMap(ProductListResult result) {
-        Stream<Map.Entry<Long, ProductContent>> mobile = result.mobilePlans().stream()
-                .map(m -> Map.entry(m.productId(), (ProductContent) toMobileContent(m)));
-        Stream<Map.Entry<Long, ProductContent>> internet = result.internets().stream()
-                .map(i -> Map.entry(i.productId(), (ProductContent) toInternetContent(i)));
-        Stream<Map.Entry<Long, ProductContent>> iptv = result.iptvs().stream()
-                .map(i -> Map.entry(i.productId(), (ProductContent) toIptvContent(i)));
-        Stream<Map.Entry<Long, ProductContent>> addon = result.addons().stream()
-                .map(a -> Map.entry(a.productId(), (ProductContent) toAddonContent(a)));
-        Stream<Map.Entry<Long, ProductContent>> tabWatch = result.tabWatchPlans().stream()
-                .map(t -> Map.entry(t.productId(), (ProductContent) toTabWatchContent(t)));
-        return Stream.of(mobile, internet, iptv, addon, tabWatch)
-                .flatMap(s -> s)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    private ProductDetailResponse toDetailResponse(ProductSummaryDto p, ProductContent content) {
+    public ProductDetailResponse toDetailResponse(ProductSummaryDto p, ProductContent content) {
         return new ProductDetailResponse(
                 p.productId(),
                 p.name(),
@@ -83,7 +45,7 @@ public class ProductResponseMapper {
         return toDetailResponse(p, content);
     }
 
-    private MobileContent toMobileContent(MobilePlanDetailDto m) {
+    public MobileContent toMobileContent(MobilePlanDetailDto m) {
         return new MobileContent(
                 m.dataAmount(),
                 m.tetheringSharingData(),
@@ -96,19 +58,19 @@ public class ProductResponseMapper {
         );
     }
 
-    private InternetContent toInternetContent(InternetDetailDto i) {
+    public InternetContent toInternetContent(InternetDetailDto i) {
         return new InternetContent(i.speedMbps(), i.addonBenefit());
     }
 
-    private IptvContent toIptvContent(IptvDetailDto i) {
+    public IptvContent toIptvContent(IptvDetailDto i) {
         return new IptvContent(i.channelCount(), i.addonBenefit());
     }
 
-    private AddonContent toAddonContent(AddonDetailDto a) {
+    public AddonContent toAddonContent(AddonDetailDto a) {
         return new AddonContent(a.addonType().name(), a.description());
     }
 
-    private TabWatchContent toTabWatchContent(TabWatchPlanDetailDto t) {
+    public TabWatchContent toTabWatchContent(TabWatchPlanDetailDto t) {
         return new TabWatchContent(t.dataAmount(), t.benefitVoiceCall(), t.benefitSms());
     }
 }

8e3bd371161194a96122d9b121eb8a26e0ac81ea
bon0512 <nonstop00450@gmail.com>
2026-02-17 02:04:06 +0900

    [HSC-39] refactor: dto 클래스 이름 수정

--- src/main/java/site/holliverse/auth/dto/SignUpResponseDto.java
+++ src/main/java/site/holliverse/auth/dto/SignUpResponseDto.java
@@ -3,7 +3,7 @@ package site.holliverse.auth.dto;
 /**
  * 회원가입 유스케이스 내부 반환 DTO.
  */
-public record SingUpResponseDto(
+public record SignUpResponseDto(
         /** 생성된 회원의 기본키. */
         Long memberId
 ) {

c7cef04751b1ec2ad8e89e8e59e301a814b59bb2
rettooo <gkdud9236@naver.com>
2026-02-16 19:25:02 +0900

    [HSC-82] feat: web dto

---
+++ src/main/java/site/holliverse/customer/web/dto/product/PlanCompareResponse.java
@@ -0,0 +1,13 @@
+package site.holliverse.customer.web.dto.product;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 요금제 비교 API 응답 body (옵션 A: current/target는 상세 API와 동일 구조).
+ */
+public record PlanCompareResponse(
+        @JsonProperty("current_plan") ProductDetailResponse currentPlan,
+        @JsonProperty("target_plan") ProductDetailResponse targetPlan,
+        ComparisonResponse comparison
+) {
+}

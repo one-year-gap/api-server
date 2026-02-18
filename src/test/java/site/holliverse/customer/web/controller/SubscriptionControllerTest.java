ecf21997a0a76f03cbe2a1b593e8a8c95e6c28e7
rettooo <gkdud9236@naver.com>
2026-02-18 21:44:50 +0900

    [HSC-103] fix: test oauth 제외

--- src/test/java/site/holliverse/customer/web/controller/SubscriptionControllerTest.java
+++ src/test/java/site/holliverse/customer/web/controller/SubscriptionControllerTest.java
@@ -29,7 +29,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         controllers = SubscriptionController.class,
         excludeAutoConfiguration = {
                 org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+                org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientWebSecurityAutoConfiguration.class
         }
 )
 @ActiveProfiles("customer")

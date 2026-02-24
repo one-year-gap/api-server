21fe42c427fdbcc90dbde09be860d4a504c1de51
tkv00 <tkv0098@gmail.com>
2026-02-25 04:42:06 +0900

    [HSC-147] fix: 오탈자 수정

--- src/main/java/site/holliverse/admin/web/dto/counsel/CounselTrafficDailyResponseDto.java
+++ src/main/java/site/holliverse/admin/web/dto/counsel/CounselTrafficDailyResponseDto.java
@@ -3,16 +3,16 @@ package site.holliverse.admin.web.dto.counsel;
 import site.holliverse.admin.query.dao.CounselTrafficDailyRawData;
 import java.util.List;
 
-public record CounselTrafficTDailyResponseDto(
+public record CounselTrafficDailyResponseDto(
         List<CounselTrafficDailyRawData> items ,
         Integer maxCount
 ) {
-    public static CounselTrafficTDailyResponseDto of(List<CounselTrafficDailyRawData> data) {
+    public static CounselTrafficDailyResponseDto of(List<CounselTrafficDailyRawData> data) {
         int maxCount = data.stream()
                 .mapToInt(CounselTrafficDailyRawData::count)
                 .max()
                 .orElse(0);
 
-        return new CounselTrafficTDailyResponseDto(data,maxCount);
+        return new CounselTrafficDailyResponseDto(data,maxCount);
     }
 }

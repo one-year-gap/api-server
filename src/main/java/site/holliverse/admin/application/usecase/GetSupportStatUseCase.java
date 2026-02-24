18debe83fc7dbc47ca374a8e20895e67776a470f
YeongHyeonHeo <djdjfug@naver.com>
2026-02-24 19:58:22 +0900

    [HSC-148] feat: 상담 처리 현황 통계 UseCase 구현

---
+++ src/main/java/site/holliverse/admin/application/usecase/GetSupportStatUseCase.java
@@ -0,0 +1,19 @@
+package site.holliverse.admin.application.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.holliverse.admin.query.dao.AdminSupportStatDao;
+import site.holliverse.admin.query.dao.AdminSupportStatRowData;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GetSupportStatUseCase {
+
+    private final AdminSupportStatDao adminSupportStatDao;
+
+    public AdminSupportStatRowData execute() {
+        return adminSupportStatDao.getSupportStatusStats();
+    }
+}

924fc7db2fc5acb255e03d1202173a7a764a0c72
rettooo <gkdud9236@naver.com>
2026-03-04 14:47:42 +0900

    [HSC-188] feat: 최근 사용량 조회

---
+++ src/main/java/site/holliverse/customer/persistence/repository/UsageMonthlyRepository.java
@@ -0,0 +1,13 @@
+package site.holliverse.customer.persistence.repository;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.holliverse.customer.persistence.entity.UsageMonthly;
+
+import java.util.Optional;
+
+@Profile("customer")
+public interface UsageMonthlyRepository extends JpaRepository<UsageMonthly, Long> {
+
+    Optional<UsageMonthly> findFirstBySubscription_IdOrderByYyymmDesc(Long subscriptionId);
+}

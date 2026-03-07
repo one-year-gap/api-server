3b983f508bccf5d72e48be8e09b8c3b527fbaec2
YeongHyeonHeo <djdjfug@naver.com>
2026-03-07 20:37:11 +0900

    [HSC-229] feat: 아웃박스 테이블 type 필드 추가 & Update문 개선

--- src/main/resources/db/migration/V19__add_security_addon_type_and_claim_token.sql
+++ src/main/resources/db/migration/V19__add_security_addon_type_and_claim_token.sql
@@ -4,3 +4,12 @@ ALTER TYPE addon_type_enum ADD VALUE 'SECURITY';
 -- 2. 상담 분석 내역 테이블에 선점 토큰(claim_token) 컬럼 추가
 ALTER TABLE consultation_analysis
     ADD COLUMN claim_token BIGINT;
+
+-- 3. 아웃박스 type 구분을 위한 새로운 ENUM 생성
+CREATE TYPE dispatch_outbox_type AS ENUM ('REQUEST', 'RESPONSE');
+
+-- 4. 아웃박스 테이블 컬럼 삭제 및 추가
+ALTER TABLE analysis_dispatch_outbox
+    DROP COLUMN efs_path_counsel,
+    DROP COLUMN efs_path_alias,
+    ADD COLUMN type dispatch_outbox_type NOT NULL;
--- src/main/resources/db/migration/V20__update_security_addon_data.sql
+++ src/main/resources/db/migration/V20__update_security_addon_data.sql
@@ -1,26 +1,25 @@
--- 기존 FAMILY_CARE(가족케어) 중 보안/인증 관련 서비스를 SECURITY로 분리
+-- 기존 FAMILY_CARE(가족케어) 중 보안/인증 관련 서비스를 SECURITY로 분리 (성능 최적화 Join 방식)
 UPDATE addon_service
 SET addon_type = 'SECURITY'
-WHERE product_id IN (
-    SELECT product_id FROM product
-    WHERE product_code IN (
-        'ADDON_FAM_001', -- 딥페이크안심보호
-        'ADDON_FAM_003', -- 스팸차단후후
-        'ADDON_FAM_007', -- 스마트안티피싱
-        'ADDON_FAM_008', -- 휴대폰번호 보호서비스
-        'ADDON_FAM_009', -- 스마트피싱보호
-        'ADDON_FAM_010', -- 오토콜
-        'ADDON_FAM_011', -- 원키퍼
-        'ADDON_FAM_012', -- 금융사고안심팩
-        'ADDON_FAM_013', -- 로그인플러스
-        'ADDON_FAM_014', -- 전화번호안심로그인
-        'ADDON_FAM_015', -- 간편결제매니저
-        'ADDON_FAM_016', -- 모션키
-        'ADDON_FAM_017', -- 유비키인증서
-        'ADDON_FAM_018', -- 모바일안전결제(ISP)
-        'ADDON_FAM_019', -- USIM 스마트인증(라온시큐어)
-        'ADDON_FAM_020', -- 스마트공동인증(드림시큐리티)
-        'ADDON_FAM_021', -- 스마트 안전결제
-        'ADDON_FAM_022'  -- MyOTP
-    )
+FROM product
+WHERE addon_service.product_id = product.product_id
+  AND product.product_code IN (
+    'ADDON_FAM_001', -- 딥페이크안심보호
+    'ADDON_FAM_003', -- 스팸차단후후
+    'ADDON_FAM_007', -- 스마트안티피싱
+    'ADDON_FAM_008', -- 휴대폰번호 보호서비스
+    'ADDON_FAM_009', -- 스마트피싱보호
+    'ADDON_FAM_010', -- 오토콜
+    'ADDON_FAM_011', -- 원키퍼
+    'ADDON_FAM_012', -- 금융사고안심팩
+    'ADDON_FAM_013', -- 로그인플러스
+    'ADDON_FAM_014', -- 전화번호안심로그인
+    'ADDON_FAM_015', -- 간편결제매니저
+    'ADDON_FAM_016', -- 모션키
+    'ADDON_FAM_017', -- 유비키인증서
+    'ADDON_FAM_018', -- 모바일안전결제(ISP)
+    'ADDON_FAM_019', -- USIM 스마트인증(라온시큐어)
+    'ADDON_FAM_020', -- 스마트공동인증(드림시큐리티)
+    'ADDON_FAM_021', -- 스마트 안전결제
+    'ADDON_FAM_022'  -- MyOTP
 );

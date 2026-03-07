42818a2e64369be8cd80cead9400d867d77b2ba6
rettooo <gkdud9236@naver.com>
2026-03-07 17:01:14 +0900

    [HSC-225] chore: Enum 대문자

--- src/main/java/site/holliverse/shared/domain/model/PersonaSegment.java
+++ src/main/java/site/holliverse/shared/domain/model/PersonaSegment.java
@@ -4,7 +4,7 @@ package site.holliverse.shared.domain.model;
  * 페르소나 추천 세그먼트 (DB CHECK 제약과 동일).
  */
 public enum PersonaSegment {
-    upsell,
-    churn_risk,
-    normal
+    UPSELL,
+    CHURN_RISK,
+    NORMAL
 }

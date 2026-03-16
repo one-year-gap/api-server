4e731bff82e78bde6f57b737c8fbc348edbdbda1
bon0512 <nonstop00450@gmail.com>
2026-03-16 17:05:50 +0900

    [HSC-318] feat: rawData 작성

--- src/main/java/site/holliverse/admin/query/dao/ChurnCouponMemberRawData.java
+++ src/main/java/site/holliverse/admin/query/dao/ChurnCouponMemberRawData.java
@@ -1,4 +1,15 @@
 package site.holliverse.admin.query.dao;
 
-public class ChurnCouponMemberRawData {
+/**
+ ==========================
+ * $NAME
+ * 쿠폰 발송 대상 회원 1명을 DB에서 조회했을 때 담는 원본 데이터 객체
+ * @author nonstop
+ * @version 1.0.0
+ * @since 2026-03-16
+ * ========================== */
+public record ChurnCouponMemberRawData(
+        Long memberId,
+        String role
+) {
 }

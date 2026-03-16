a31d555bb24b184a6a3166bbc03503270aa1b13d
bon0512 <nonstop00450@gmail.com>
2026-03-16 17:06:10 +0900

    [HSC-318] feat: 쿠폰 rawdata 작성

--- src/main/java/site/holliverse/admin/query/dao/CouponRawData.java
+++ src/main/java/site/holliverse/admin/query/dao/CouponRawData.java
@@ -1,4 +1,19 @@
 package site.holliverse.admin.query.dao;
 
-public class CouponRawData {
+import java.time.LocalDateTime;
+
+
+/**
+ ==========================
+ * $NAME
+ * 쿠폰아이디 ,90일 , 엔드기간
+ * @author nonstop
+ * @version 1.0.0
+ * @since 2026-03-16
+ * ========================== */
+public record CouponRawData(
+        Long couponId,
+        Integer validDays,
+        LocalDateTime validEndDate
+) {
 }

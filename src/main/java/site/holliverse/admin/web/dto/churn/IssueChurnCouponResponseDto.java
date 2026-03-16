b6c6b3de8e62cbc18fce386ec394c2b727f37145
bon0512 <nonstop00450@gmail.com>
2026-03-16 17:04:31 +0900

    [HSC-318] feat: dto 작성

--- src/main/java/site/holliverse/admin/web/dto/churn/IssueChurnCouponResponseDto.java
+++ src/main/java/site/holliverse/admin/web/dto/churn/IssueChurnCouponResponseDto.java
@@ -1,4 +1,34 @@
 package site.holliverse.admin.web.dto.churn;
 
-public record IssueChurnCouponResponseDto() {
+import lombok.Builder;
+
+import java.util.List;
+
+/**
+ ==========================
+ * $NAME
+ * 쿠폰 발급 응답 dto
+ *
+ * @author nonstop
+ * @version 1.0.0
+ * @since 2026-03-16
+ * ========================== */
+@Builder
+public record IssueChurnCouponResponseDto(
+
+        //요청된 멤버 카운트
+        int requestedCount,
+
+        // 발급된 멤버 카운트
+        int issuedCount,
+
+        // 발급 생략된 멤버 카운트
+        int skippedCount,
+
+        //발급된 멤버들
+        List<Long> issuedMemberIds,
+        //발급 생략된 멤버들
+        List<SkippedCouponIssueMemberDto> skippedMembers
+
+) {
 }

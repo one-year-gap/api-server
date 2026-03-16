a010567b045411e263fef9902f8e61adea241244
rettooo <gkdud9236@naver.com>
2026-03-16 11:19:08 +0900

    [HSC-313] feat: 스냅샷 없을때 생성 정책 DAO

--- src/main/java/site/holliverse/admin/query/dao/MemberActionFeatureLogDao.java
+++ src/main/java/site/holliverse/admin/query/dao/MemberActionFeatureLogDao.java
@@ -3,10 +3,13 @@ package site.holliverse.admin.query.dao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import site.holliverse.admin.query.jooq.enums.FeatureType;
+import site.holliverse.admin.query.jooq.tables.records.FeatureSnapshotStoreRecord;
 
 import static site.holliverse.admin.query.jooq.Tables.FEATURE_SNAPSHOT_STORE;
 import static site.holliverse.admin.query.jooq.Tables.MEMBER_ACTION_FEATURE;
@@ -14,10 +17,13 @@ import static site.holliverse.admin.query.jooq.Tables.MEMBER_ACTION_FEATURE;
 /**
  * member_action_feature의 comparison_cnt / checked_penalty_fee_cnt 실시간 증분 갱신용 DAO.
  * feature_snapshot_store에서 해당 회원·MEMBER_ACTION_FEATURE 최신 스냅샷 조회 후 갱신.
+ * 스냅샷 없을 때 생성 정책: 시퀀스로 새 ID 발급 후 feature_snapshot_store + member_action_feature 1건씩 INSERT.
  */
 @Profile("admin")
 @RequiredArgsConstructor
 public class MemberActionFeatureLogDao {
+
+    private static final String FEATURE_SNAPSHOT_ID_SEQ = "feature_snapshot_id_seq";
 
     private final DSLContext dsl;
 
